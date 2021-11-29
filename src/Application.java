3348a82e57859210c5b5b9474a84e4c682a74f04
Robin Wollenschlaeger <r.wollenschlaeger@goettfert.de>
2021-11-29 20:50:25 +0100

    started coding

---
+++ src/Application.java
@@ -0,0 +1,5 @@
+public class Application {
+
+   FLF flf = new FLF.Builder.build();
+
+}
---
+++ src/FLF.java
@@ -0,0 +1,21 @@
+/**
+ *
+ */
+public class FLF {
+
+    /**
+     *
+     */
+    public static class Builder{
+
+        public  Builder(){
+
+        }
+
+        /**
+         *
+         * @return FLF
+         */
+        public FLF build(){return new FLF(this);};
+    }
+}
