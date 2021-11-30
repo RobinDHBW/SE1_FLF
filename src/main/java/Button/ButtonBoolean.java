6c0015fe5ffd0ead045a0d3891d432377a267aaf
Robin Wollenschlaeger <r.wollenschlaeger@goettfert.de>
2021-11-30 08:48:11 +0100

    implement BooleanButton family

--- src/main/java/Button/ButtonBoolean.java
+++ src/main/java/Button/ButtonBoolean.java
@@ -1,10 +1,24 @@
 package Button;
 
-public abstract class ButtonBoolean {
-    private Boolean toggleState = false;
+public abstract class ButtonBoolean implements IButtonBoolean{
+    protected Boolean toggleState = false;
 
     public ButtonBoolean(){
 
     }
 
+    @Override
+    public void turnOn() {
+        this.toggleState=true;
+    }
+
+    @Override
+    public void turnOff() {
+        this.toggleState=false;
+    }
+
+    @Override
+    public Boolean isOn(){
+        return toggleState;
+    }
 }
