8ae27e25c39a1b7d40ee379c418baa27b021ccd4
Robin Wollenschlaeger <r.wollenschlaeger@goettfert.de>
2021-11-30 11:33:34 +0100

    documented busdoor TODO

--- src/main/java/Cabin/Busdoor.java
+++ src/main/java/Cabin/Busdoor.java
@@ -1,9 +1,16 @@
 package Cabin;
 
+import Button.ButtonPush;
+
 public class Busdoor {
     private final VehicleSide side;
     private Boolean isOpen = false;
-    
+
+    /**
+     * @TODO Person should interact with button to open door -> Maybe eventListener
+     */
+    public ButtonPush btnPushOutside = new ButtonPush();
+    public ButtonPush btnPushInside = new ButtonPush();
 
     public Busdoor(VehicleSide side) {
         this.side = side;
