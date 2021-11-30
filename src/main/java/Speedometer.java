f9564afa30666bc960d6d4adee75bf973f9684d1
Robin Wollenschlaeger <r.wollenschlaeger@goettfert.de>
2021-11-30 08:05:32 +0100

    add Speedometer and steering wheel

---
+++ src/main/java/Speedometer.java
@@ -0,0 +1,10 @@
+public class Speedometer {
+    private Integer speed = 0;
+    public Speedometer(){
+
+    }
+
+    public void setSpeed(Integer speed) {
+        this.speed = speed;
+    }
+}
---
+++ src/main/java/SteeringWheel.java
@@ -0,0 +1,14 @@
+public class SteeringWheel {
+    private Double degree = 0.0;
+    public SteeringWheel(){
+
+    }
+
+    public void setDegree(Double degree) {
+        this.degree = degree;
+    }
+
+    public Double getDegree() {
+        return degree;
+    }
+}
