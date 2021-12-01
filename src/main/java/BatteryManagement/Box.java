9eb09d44b27f32428d12b291c587857f716ef440
Robin Wollenschlaeger <r.wollenschlaeger@goettfert.de>
2021-12-01 10:59:01 +0100

    construct box and calc average batterie fill state

--- src/main/java/BatteryManagement/Box.java
+++ src/main/java/BatteryManagement/Box.java
@@ -1,7 +1,29 @@
 package BatteryManagement;
 
+import java.util.ArrayList;
+
 public class Box {
-    public int takeout(int amount) {
+    Integer width, height;
+    private ArrayList<Battery> batteryStore = new ArrayList<>();
+
+    public Box(Integer width, Integer height) {
+        this.width = width;
+        this.height = height;
+
+        for (int i = 0; i < width * height; i++) {
+            batteryStore.add(new Battery(BatteryUnit.POSITIVE, 100, 10, 100));
+        }
+    }
+
+    public Integer takeout(int amount) {
         return amount;
+
+    }
+
+    public Double getRelativeFillState() {
+        return batteryStore.stream()
+                .mapToDouble(x -> x.getRelativeFillState())
+                .average()
+                .orElse(0);
     }
 }
