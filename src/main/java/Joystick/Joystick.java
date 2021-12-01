17b36c0b3dfa6520231884d00c95f9884dc23bc4
Robin Wollenschlaeger <r.wollenschlaeger@goettfert.de>
2021-12-01 14:55:32 +0100

    changed UML, add listener to Joystick

--- src/main/java/Joystick/Joystick.java
+++ src/main/java/Joystick/Joystick.java
@@ -2,14 +2,21 @@ package Joystick;
 
 import Button.ButtonPress;
 import Button.ButtonPush;
+import Button.IButtonListener;
 
-public class Joystick {
+public class Joystick implements IButtonListener {
     public ButtonPush btnPush = new ButtonPush();
     public ButtonPress btnPressLeft = new ButtonPress();
     public ButtonPress btnPressRight = new ButtonPress();
 
     public Joystick(){
-
+        btnPush.addListener(this);
+        btnPressLeft.addListener(this);
+        btnPressRight.addListener(this);
     }
 
+    @Override
+    public void onToggleButton(Object o) {
+        //@TODO Alle Zustände abfragen -> Was ist zu tun?
+    }
 }
