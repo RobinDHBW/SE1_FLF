fb3a9367e77b451757ac31178df244e8592f3851
Robin Wollenschlaeger <r.wollenschlaeger@goettfert.de>
2022-02-01 11:09:09 +0100

    equip driver and operator

--- src/main/java/Person/Driver.java
+++ src/main/java/Person/Driver.java
@@ -1,8 +1,24 @@
 package Person;
 
+import Button.Pedal;
+import Cabin.ControlPanel;
+import Instruments.SteeringWheel;
+import Joystick.Joystick;
+
 public class Driver extends Person {
+    private SteeringWheel steeringWheel;
+    private Pedal gasPedal;
+    private Pedal brakePedal;
+    private Joystick drJoystick;
 
     public Driver() {
 
+    }
+
+    public void equip(SteeringWheel wheel, Pedal gas, Pedal brake, Joystick stick){
+        this.steeringWheel = wheel;
+        this.gasPedal = gas;
+        this.brakePedal = brake;
+        this.drJoystick = stick;
     }
 }
--- src/main/java/Person/Operator.java
+++ src/main/java/Person/Operator.java
@@ -1,6 +1,17 @@
 package Person;
 
+import Cabin.ControlPanel;
+import Joystick.Joystick;
+
 public class Operator extends Person {
+    private ControlPanel panel;
+    private Joystick opJoystick;
+
     public Operator() {
+    }
+
+    public void equip(ControlPanel panel, Joystick opJoystick){
+        this.panel =panel;
+        this.opJoystick=opJoystick;
     }
 }
