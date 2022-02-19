cb9e7ea76bb7a213fb9a108c1c5b613c37e09731
Robin Wollenschlaeger <r.wollenschlaeger@goettfert.de>
2022-02-19 18:24:37 +0100

    DES encryption

--- src/main/java/CentralUnit/CryptoUnit.java
+++ src/main/java/CentralUnit/CryptoUnit.java
@@ -4,6 +4,10 @@ import java.math.BigInteger;
 import java.nio.charset.StandardCharsets;
 
 public class CryptoUnit {
+    private final KeySchedule keySchedule = new KeySchedule();
+    private final InitialPermutation initialPermutation = new InitialPermutation();
+    private final FeistelNetwork feistelNetwork = new FeistelNetwork();
+    private final IPInverse ipInverse = new IPInverse();
 
     private String stringToBit(String input){
        return new BigInteger(input.getBytes(StandardCharsets.UTF_8)).toString(2);
@@ -12,11 +16,13 @@ public class CryptoUnit {
     private String bitToString(String input){
         return new String(new BigInteger(input, 2).toByteArray());
     }
-
-    //@TODO write logic
+    
     public String decrypt(String cipher, String key){
-        String BitCipher = stringToBit(cipher);
-        return null;
+        return this.ipInverse.permute(this.feistelNetwork.iterate(this.initialPermutation.permute(stringToBit(cipher)),this.keySchedule.schedule(key, false)));
+    }
+
+    public String encrpyt(String plain, String key){
+        return this.ipInverse.permute(this.feistelNetwork.iterate(this.initialPermutation.permute(stringToBit(plain)),this.keySchedule.schedule(key, true)));
     }
 
 }
