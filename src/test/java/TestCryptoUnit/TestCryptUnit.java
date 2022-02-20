b5d71a43e252b91d6143e168954775ee47fb08aa
Robin Wollenschlaeger <r.wollenschlaeger@goettfert.de>
2022-02-20 22:01:04 +0100

    still same problem...

--- src/test/java/TestCryptoUnit/TestCryptUnit.java
+++ src/test/java/TestCryptoUnit/TestCryptUnit.java
@@ -33,15 +33,15 @@ public class TestCryptUnit {
     @TestFactory
     Stream<DynamicTest> testCryptoWithProjectData() {
         ArrayList<DynamicTest> tests = new ArrayList<>();
-        String plain = "DUS | FLF-5";
+        String plain = "FT-DUS-FLF-5";
         String key = "6072";
 
         String cipher = cryptoUnit.encrypt(plain, key);
 
         Collections.addAll(tests,
-                DynamicTest.dynamicTest("check string to bit", ()-> assertEquals(cryptoUnit.pushToNextByte("100010001010101010100110010000001111100001000000100011001001100010001100010110100110101"), cryptoUnit.stringToBit(plain))),
+                DynamicTest.dynamicTest("check string to bit", ()-> assertEquals(cryptoUnit.pushToNextByte("010001100101010000101101010001000101010101010011001011010100011001001100010001100010110100110101"), cryptoUnit.stringToBit(plain))),
                 DynamicTest.dynamicTest("check bit to string", ()-> assertEquals(plain, cryptoUnit.bitToString(cryptoUnit.stringToBit(plain)))),
-                DynamicTest.dynamicTest("check cipher", ()-> assertEquals("0100010001010101010110011000000011110100100000000100110001100100", cipher)),
+                DynamicTest.dynamicTest("check cipher", ()-> assertEquals("01101101101000010001100100100001011010111011110101000001010110100010111010011101001100010010001101100010000010010000101100011010", cipher)),
                 DynamicTest.dynamicTest("check decrypted plain", ()-> assertEquals(plain, this.cryptoUnit.decrypt(cipher, key)))
         );
         return tests.stream();
